ba7791c5cf162d542317709a8da83312e1dfcd4f
jack1142 <6032823+jack1142@users.noreply.github.com>
2019-05-19 19:19:04 +0200

    build: Change handling of version number to work without imports
    add separate file for version number

--- rlapi/__init__.py
+++ rlapi/__init__.py
@@ -1,9 +1,10 @@
 __author__ = 'Jakub Kuczys (jack1142)'
 
-__version__ = '0.1.0a1'
-
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2019 Jakub Kuczys'
+
+import pkgutil
+__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
 
 import logging
 
@@ -14,3 +15,5 @@ from .enums import Platform, PlaylistKey  # noqa
 from .player import Playlist, SeasonRewards, Player  # noqa
 
 log = logging.getLogger(__name__)
+
+del pkgutil
