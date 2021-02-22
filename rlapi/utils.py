746d9f22562dc93c423b064070093934865bc93a
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-02-22 23:43:46 +0100

    fix(tier_breakdown): update Tracker Network provider module (#64)

--- rlapi/utils.py
+++ rlapi/utils.py
@@ -14,45 +14,21 @@
 
 import json
 import sys
-from typing import Any, Callable
+from typing import Any
 
 import aiohttp
-from lxml import etree
 
 if sys.version_info[:2] >= (3, 8):
     from typing import Literal
 else:
     from typing_extensions import Literal
 
-__all__ = ("json_or_text", "stringify")
-
-
-_stringify: Callable[[etree._Element], str] = etree.XPath(  # type: ignore
-    "string()", smart_strings=False
-)
+__all__ = ("json_or_text",)
 
 
 class AlwaysGreaterOrEqual:
     def __ge__(self, other: Any) -> Literal[True]:
         return True
-
-
-def stringify(element: etree._Element) -> str:
-    """
-    Returns element node's text, stripped from whitespace.
-
-    Parameters
-    ----------
-    element: `etree._Element`
-        Element node object.
-
-    Returns
-    -------
-    `str`
-        Element node's text.
-
-    """
-    return _stringify(element).strip()
 
 
 async def json_or_text(resp: aiohttp.ClientResponse) -> Any:
